3a817ab785ad9333cc10dd780f1abe458efd2de4
Mohamed Ahmed <memoahmed412000@gmail.com>
2023-11-13 11:43:20 +0200

    show message in response

--- src/main/java/spring/boot/tutorial/demo/customer/CustomerService.java
+++ src/main/java/spring/boot/tutorial/demo/customer/CustomerService.java
@@ -19,7 +19,8 @@ public class CustomerService {
     Customer getCustomer(Long id) {
         return getCustomers().stream()
             .filter(customer -> customer.getId().equals(id))
-            .findFirst().orElseThrow(() -> new IllegalStateException("Customer not found"));
+            .findFirst().orElseThrow(() -> 
+                new IllegalStateException("Customer with id=" + id + " not found"));
     }
 
 }
