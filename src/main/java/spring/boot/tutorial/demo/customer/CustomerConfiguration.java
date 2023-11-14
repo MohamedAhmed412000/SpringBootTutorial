a7991aee8b85aea61eab839addf60a770bce369a
Mohamed Ahmed <memoahmed412000@gmail.com>
2023-11-14 15:53:47 +0200

    Using Faker to add random data, More Sql syntax using JPA

--- src/main/java/spring/boot/tutorial/demo/customer/CustomerConfiguration.java
+++ src/main/java/spring/boot/tutorial/demo/customer/CustomerConfiguration.java
@@ -4,6 +4,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.ScrollPosition.Direction;
+
+import com.github.javafaker.Faker;
 
 @Configuration
 public class CustomerConfiguration {
@@ -12,14 +16,46 @@ public class CustomerConfiguration {
 
     @Bean
     CommandLineRunner commandLineRunner(CustomerRepository customerRepository) {
+        // Only for testing purposes
         return args -> {
-            customerRepository.findCustomerByEmail("mohamed.ahmed04012000@gmail.com")
-                .ifPresentOrElse(System.out::println, () -> System.out.println("Not found"));
+            // customerRepository.findCustomerByEmail("mohamed.ahmed04012000@gmail.com")
+            //     .ifPresentOrElse(System.out::println, () -> System.out.println("Not found"));
 
-            System.out.println(
-                customerRepository.filterCustomersUsingFirstNameAndAge("Abdo", 23)
-            );
+            // System.out.println(
+            //     customerRepository.filterCustomersUsingFirstNameAndAge("Abdo", 23)
+            // );
+
+            // System.out.println(
+            //     customerRepository.filterCustomersUsingFirstNameAndAgeNative(
+            //         "Abdo", 
+            //         23
+            //     )
+            // );
+            
+            generateCustomers(customerRepository, 20);
+            // Multi sorting example
+            Sort sort = Sort.by("firstname").ascending()
+                            .and(Sort.by("lastname").ascending())
+                            .and(Sort.by("age")).descending();
+            customerRepository.findAll(sort)
+                .forEach(customer -> System.out.println(customer));
         };
+    }
+
+    void generateCustomers(CustomerRepository customerRepository, Integer num) {
+        Faker faker = new Faker();
+        for (int i = 0; i < num; i++) {
+            String firstName = faker.name().firstName();
+            String lastName = faker.name().lastName();
+            String email = String.format("%s.%s@gmail.com", firstName, lastName);
+            Integer age = faker.number().numberBetween(17, 45);
+
+            Customer customer = new Customer(
+                firstName, lastName, email, age, "123456"
+            );
+            customerRepository.save(customer);
+
+        }
     }
 
     // @Bean
--- src/main/java/spring/boot/tutorial/demo/customer/CustomerRepository.java
+++ src/main/java/spring/boot/tutorial/demo/customer/CustomerRepository.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -12,8 +13,19 @@ public interface CustomerRepository extends JpaRepository<Customer, Long> {
     @Query("SELECT c FROM Customer c WHERE c.email = ?1")
     Optional<Customer> findCustomerByEmail(String email);
 
+    // This doesn't depend on database
     @Query("SELECT c FROM Customer c WHERE c.firstname = ?1 AND c.age >= ?2")
     List<Customer> filterCustomersUsingFirstNameAndAge(
         String firstName, Integer age
+    );
+    
+    // This differs from Database to another
+    @Query(
+        value = "SELECT * FROM Customer WHERE first_name = :firstName AND age >= :age",
+        nativeQuery = true    
+    )
+    List<Customer> filterCustomersUsingFirstNameAndAgeNative(
+        @Param(value = "firstName") String firstName, 
+        @Param(value = "age") Integer age
     );
 }
